e3191010216f004976718187973bb3e04170c4e8
Carl Jackson <carl@avtok.com>
2012-12-19 02:18:53 -0500

    Beginnings of a config server

---
+++ lib/servers/config.rb
@@ -0,0 +1,18 @@
+class ConfigServer < Server
+  INSTANCE_TYPE = 't1.micro'
+  IP_RANGE = 4..9
+
+  def ssh_hook ssh
+    super
+
+    # XXX: We should switch to the normal Bcfg2 PPA when 1.3.0 is released
+    ssh.stream 'sudo add-apt-repository ppa:bcfg2/precisetesting'
+    ssh.stream 'sudo apt-get update'
+
+    ssh.stream 'sudo apt-get install -y bcfg2-server bcfg2 git'
+    ssh.stream 'sudo rm -r /var/lib/bcfg2'
+    ssh.stream 'sudo git clone https://github.com/hcs/config.git /var/lib/bcfg2'
+
+    # TODO: run the bootstrap script (once it works!)
+  end
+end
