a70e107d2669ec255d30146edc08a7534dfe7167
Carl Jackson <carl@avtok.com>
2012-12-21 15:28:27 -0500

    File server!

---
+++ lib/servers/file.rb
@@ -0,0 +1,11 @@
+class FileServer < Server
+  INSTANCE_TYPE = 'm1.large'
+  IP_RANGE = 250..253
+
+  def ssh_hook ssh
+    ssh.stream 'sudo add-apt-repository ppa:zfs-native/stable'
+    ssh.stream 'sudo apt-get update'
+
+    ssh.stream 'sudo apt-get install -y ubuntu-zfs'
+  end
+end
