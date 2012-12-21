5e8101a3e327f7bebbed9ea60143a5ee8289cf5c
Carl Jackson <carl@avtok.com>
2012-12-21 16:31:23 -0500

    Oops

--- lib/servers/file.rb
+++ lib/servers/file.rb
@@ -3,6 +3,8 @@ class FileServer < Server
   IP_RANGE = 250..253
 
   def ssh_hook ssh
+    super
+
     ssh.stream 'sudo add-apt-repository ppa:zfs-native/stable'
     ssh.stream 'sudo apt-get update'
 
