7b978ba8b61399d8e25b700aee785da5ae2884e5
Carl Jackson <carl@avtok.com>
2013-01-16 13:35:21 -0500

    Login server

---
+++ lib/servers/login.rb
@@ -0,0 +1,13 @@
+class LoginServer < Server
+  IP_RANGE = 20...30
+  SECURITY_GROUPS = ['sg-791dec16']
+  PUBLIC = true
+
+  def create_hook
+    $log.info "Allocating an IP address for the gateway"
+    @instance.ip_address = $EC2.elastic_ips.allocate :vpc => true
+    $log.info "Allocated IP #{@instance.ip_address}"
+
+    ssh_hook
+  end
+end
