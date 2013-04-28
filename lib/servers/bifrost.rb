a12b4ef1033ba0cf5f80050d2baf06f1c6d1e7c4
Carl Jackson <carl@avtok.com>
2013-04-28 16:22:44 -0400

    Gateways are the new Bifrosts

--- lib/servers/bifrost.rb
+++ lib/servers/bifrost.rb
@@ -10,16 +10,6 @@ class BifrostServer < Server
     @instance.ip_address = $EC2.elastic_ips.allocate :vpc => true
     $log.info "Allocated IP #{@instance.ip_address}"
 
-    # Disable source/destination checking, so we can act as a NAT for other
-    # instances in the VPC.
-    @instance.network_interfaces.each do |ni|
-      ni.source_dest_check = false
-    end
-
     ssh_hook
-
-    ssh 'echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward'
-
-    $log.info "Be sure to change the routing tables to go through instance #{id}"
   end
 end
